4a77f2f6e760bb860c69a479b10d73b7627cf383
plusman <liujianan@souche.com>
2021-04-10 09:42:14 +0800

    feat: add savePoint Test

---
+++ spring-transaction/README.md
@@ -0,0 +1,4 @@
+# Spring-Transaction
+
+>示例项目使用 Embedded H2 数据库，通过 spring schema.sql 初始化数据结构，通过访问 http://localhost:8080/h2-console 管理界面，可进行基础的 CURD 查询，方便读者进行数据校验。
+当然有经验的读者，也可以把数据源切换为 mysql，毕竟其 GUI 工具更加齐全。
--- spring-transaction/src/main/java/cn/plusman/springtransaction/PlainOldJDBCSample.java
+++ spring-transaction/src/main/java/cn/plusman/springtransaction/PlainOldJDBCSample.java
@@ -1,5 +1,7 @@
 package cn.plusman.springtransaction;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.sql.*;
 
@@ -8,6 +10,7 @@ import java.sql.*;
  * @author plusman
  * @since 2021/4/9 10:43 PM
  */
+@Slf4j
 public class PlainOldJDBCSample {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("org.h2.Driver");
@@ -17,16 +20,8 @@ public class PlainOldJDBCSample {
         try {
             connection.setAutoCommit(false); // (2)
             // execute some SQL statements...
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate("drop table BOOKINGS if exists;\n" +
-                    "create table BOOKINGS(ID serial, FIRST_NAME varchar(5) NOT NULL);");
-                statement.executeUpdate("insert into bookings (FIRST_NAME) values('Gan');");
-                statement.executeUpdate("insert into bookings (FIRST_NAME) values('Liu');");
-                
-                try (ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOKINGS")) {
-                    printResult(resultSet);
-                }
-            }
+            // insertTest();
+            savepointTest(connection);
             
             connection.commit(); // (3)
         } catch (SQLException e) {
@@ -37,6 +32,71 @@ public class PlainOldJDBCSample {
         }
     }
     
+    /**
+     * 注意此处方法一定要将异常往外抛出，不然无法触发错误回滚
+     * @param connection
+     */
+    public static void insertTest(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("drop table BOOKINGS if exists;\n" +
+                "create table BOOKINGS(ID serial, FIRST_NAME varchar(5) NOT NULL);");
+            statement.executeUpdate("insert into bookings (FIRST_NAME) values('Gan');");
+            statement.executeUpdate("insert into bookings (FIRST_NAME) values('Liu');");
+        
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOKINGS")) {
+                printResult(resultSet);
+            }
+        }
+    }
+    
+    /**
+     * 保存点测试
+     * @param connection
+     */
+    public static void savepointTest(Connection connection) throws SQLException {
+        Savepoint savepoint = null;
+        
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("drop table BOOKINGS if exists;\n" +
+                "create table BOOKINGS(ID serial, FIRST_NAME varchar(5) NOT NULL);");
+            statement.executeUpdate("insert into bookings (FIRST_NAME) values('Gan');");
+            // 创建 savePoint
+            savepoint = connection.setSavepoint();
+            
+            statement.executeUpdate("insert into bookings (FIRST_NAME) values('Liu');");
+            statement.executeUpdate("insert into bookings (FIRST_NAME) values('LiuHanSan');");
+         
+            
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOKINGS")) {
+                printResult(resultSet);
+            }
+        } catch (SQLException throwables) {
+            log.error("savepointTest 发生异常错误：", throwables);
+            
+            log.info("打印结果包含 Gan、Liu");
+            try (Statement statement = connection.createStatement()) {
+                try (ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOKINGS")) {
+                    printResult(resultSet);
+                }
+            }
+    
+            log.info("即将回滚到保存点");
+            connection.rollback(savepoint);
+    
+            log.info("打印结果只包含 Gan");
+            try (Statement statement = connection.createStatement()) {
+                try (ResultSet resultSet = statement.executeQuery("SELECT * FROM BOOKINGS")) {
+                    printResult(resultSet);
+                }
+            }
+        }
+    }
+    
+    /**
+     * 打印
+     * @param rs
+     * @throws SQLException
+     */
     public static void printResult(ResultSet rs) throws SQLException {
         while(rs.next()){
             //Retrieve by column name
